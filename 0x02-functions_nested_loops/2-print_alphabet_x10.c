be7bb2e2a0c3517ed622799c48cfb4ddcd85a51e
JamesCicada <mabdoox@gmail.com>
2023-03-21 11:37:51 -0700

    Mandatory Tasks

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet 10 times
- *
- * Return: void
- */
+* print_alphabet_x10 - prints the alphabet 10 times
+*
+* Return: void
+*/
 void print_alphabet_x10(void)
 {
-    int i;
-    char c;
-
-    for (i = 0; i < 10; i++) {
-        c = 'a';
-        while (c <= 'z') {
-            _putchar(c);
-            c++;
-        }
-        _putchar('\n');
-    }
+int i;
+char c;
+	for (i = 0; i < 10; i++)
+	{
+	c = 'a';
+	while (c <= 'z') {
+	_putchar(c);
+	c++;
+	}
+	_putchar('\n');
+	}
 }
