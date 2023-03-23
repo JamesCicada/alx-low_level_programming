0ada854793bd1b20b0054e3efcd030376b568bc1
JamesCicada <mabdoox@gmail.com>
2023-03-23 03:04:09 -0700

    Mandatory tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _isupper - checks if a character is uppercase
- *
- * @c: the character to be checked
- *
- * Return: 1 if c is uppercase, 0 otherwise
+* _isupper - checks if a character is uppercase
+*
+* @c: the character to be checked
+*
+* Return: 1 if c is uppercase, 0 otherwise
 */
 
 int _isupper(int c)
