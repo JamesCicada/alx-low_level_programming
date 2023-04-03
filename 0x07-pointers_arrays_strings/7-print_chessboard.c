81e3e887a50b392a1b5cb272a662a3d56885ec27
JamesCicada <mabdoox@gmail.com>
2023-04-03 15:23:53 -0700

    Mandatory Tasks

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,21 @@
 #include "main.h"
 
 /**
- * print_chessboard - prints the chessboard
- * @a: the 2D array containing the chessboard
+ * print_chessboard - prints the chessboard.
+ * @a: the array representing the chessboard
  *
  * Return: void
  */
-void print_chessboard(char (*a))
+void print_chessboard(char (*a)[8])
 {
 	int row, col;
+	
 	for (row = 0; row < 8; row++)
 	{
 		for (col = 0; col < 8; col++)
+		{
 			_putchar(a[row][col]);
+		}
 		_putchar('\n');
 	}
 }
