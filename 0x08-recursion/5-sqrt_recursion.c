99b4b2647bfc6e88f702ec60586e21e09e6717fd
JamesCicada <mabdoox@gmail.com>
2023-04-05 10:29:13 -0700

    Mandatory Tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,42 +1,34 @@
 #include "main.h"
+/**
+ * _sqrt_recursion_helper - recursive helper function to find the square root of a number
+ * @n: the number to find the square root of
+ * @root: the current root value
+ * @prev: the previous root value
+ *
+ * Return: the square root of n, or -1 if n does not have a natural square root
+ */
+int _sqrt_recursion_helper(int n, int root, int prev)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	if (root * root == n)
+		return (root);
+	if (root == prev)
+		return (-1);
+	
+	return (_sqrt_recursion_helper(n, (root + n / root) / 2, root));
+}
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: the number to find the square root of
  *
- * Return: the natural square root of n, or -1 if n does not have a
- * natural square root
+ * Return: the square root of n, or -1 if n does not have a natural square root
  */
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    if (n == 0 || n == 1)
-        return (n);
-    return (find_sqrt(n, 1, n));
-}
-
-/**
- * find_sqrt - recursively finds the natural square root of a number
- * within a range
- * @n: the number to find the square root of
- * @min: the minimum possible square root
- * @max: the maximum possible square root
- *
- * Return: the natural square root of n, or -1 if n does not have a
- * natural square root within the range [min, max]
- */
-int find_sqrt(int n, int min, int max)
-{
-    int mid;
-
-    if (min > max)
-        return (-1);
-    mid = (min + max) / 2;
-    if (mid * mid == n)
-        return (mid);
-    if (mid * mid < n)
-        return (find_sqrt(n, mid + 1, max));
-    return (find_sqrt(n, min, mid - 1));
+	return (_sqrt_recursion_helper(n, n / 2, 0));
 }
 
