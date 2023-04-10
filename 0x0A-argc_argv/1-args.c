1b4c3374a12c4c6b69ecf91ddf64a3da976570eb
JamesCicada <mabdoox@gmail.com>
2023-04-10 09:43:46 -0700

    Mandatory Tasks

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into the program
+ * @argc: number of arguments passed to the program
+ * @argv: array of strings containing the arguments passed to the program
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+    (void)argv;
+    printf("%d\n", argc);
+    return (0);
+}
+
