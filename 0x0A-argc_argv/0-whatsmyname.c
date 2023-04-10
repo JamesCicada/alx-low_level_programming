e893d8a185a82e7650aa906120567dcf066aaf12
JamesCicada <mabdoox@gmail.com>
2023-04-10 09:38:56 -0700

    Mandatory Tasks

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * main - Entry point
- * @argc: Number of arguments passed to the program
- * @argv: Array of strings containing the arguments passed to the program
+ * main - prints the name of the program
+ * @argc: number of arguments passed to the program
+ * @argv: array of strings containing the arguments passed to the program
  *
- * Return: Always 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
