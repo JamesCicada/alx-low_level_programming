4f48f3e5220e22e68111dca21ccf09e5668bdb06
JamesCicada <mabdoox@gmail.com>
2023-06-21 11:29:34 -0700

    Mandatory Tasks

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "function_pointers.h"
 
 /**
  * print_name - Prints a given name using the provided function pointer.
@@ -9,7 +10,57 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+    if (name != NULL && f != NULL)
+        f(name);
+}
+
+/**
+ * print_name_as_is - Prints a name as is.
+ * @name: Name of the person.
+ *
+ * Return: void
+ */
+void print_name_as_is(char *name)
+{
+    printf("Hello, my name is %s\n", name);
+}
+
+/**
+ * print_name_uppercase - Prints a name in uppercase.
+ * @name: Name of the person.
+ *
+ * Return: void
+ */
+void print_name_uppercase(char *name)
+{
+    unsigned int i;
+
+    printf("Hello, my uppercase name is ");
+    i = 0;
+    while (name[i])
+    {
+        if (name[i] >= 'a' && name[i] <= 'z')
+        {
+            putchar(name[i] + 'A' - 'a');
+        }
+        else
+        {
+            putchar(name[i]);
+        }
+        i++;
+    }
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_name("Bob", print_name_as_is);
+    print_name("Bob Dylan", print_name_uppercase);
+    printf("\n");
+    return 0;
 }
 
