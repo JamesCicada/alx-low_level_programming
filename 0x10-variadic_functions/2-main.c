d6d890b23a932a9a70952cb6f8693985e7fcc6ee
JamesCicada <mabdoox@gmail.com>
2023-06-22 12:05:57 -0700

    Mandatory Tasks

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_strings(", ", 2, "Jay", "Django");
+	return (0);
+}
+
