05e28f049eda8d63c4f0c343de93451bf27b89f7
JamesCicada <mabdoox@gmail.com>
2023-06-30 10:23:10 -0700

    mandatory tasks

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,7 +5,7 @@
 /**
  * add_node_end - Adds a new node at the end of a list_t list.
  * @head: Pointer to a pointer to the head of the list.
- * @str: String to be stored in the new node.
+ * @str: String to be duplicated and added to the list.
  *
  * Return: Address of the new element, or NULL if it failed.
  */
@@ -23,22 +23,21 @@ list_t *add_node_end(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
+
 	new_node->len = strlen(str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = new_node;
+		return (new_node);
 	}
-	else
-	{
 
-		temp = *head;
-		while (temp->next != NULL)
-			temp = temp->next;
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
 
-		temp->next = new_node;
-	}
+	temp->next = new_node;
 
 	return (new_node);
 }
