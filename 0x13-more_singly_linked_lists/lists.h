405dc5f1c22b783f5f0d4d48e5523e01bc27ac49
JamesCicada <mabdoox@gmail.com>
2023-07-04 11:04:03 -0700

    Advanced Tasks

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,7 +2,9 @@
 #define LISTS_H
 
 #include <stddef.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -29,6 +31,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
 
 #endif /* LISTS_H */
 
