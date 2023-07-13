b9067f46a106db8ddfc7f3cd701e7da1b4a1e6dc
JamesCicada <mabdoox@gmail.com>
2023-07-13 05:32:20 -0700

    Mandaory Tasks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Copies the contents of a file to another file
- * @argc: Number of arguments
- * @argv: Argument vector
- *
+ * @argc: number of arg
+ * @argv: argument
  * Return: 0 on success
  */
+
 int main(int argc, char **argv)
 {
 	int f1, f2, r, w, o1, o2;
@@ -15,56 +15,48 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
+		exit (97);
 	}
-
-	buffer = malloc(sizeof(char) * 1024);
+	buffer = malloc (sizeof(char) * 1024);
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-
 	f1 = open(argv[1], O_RDONLY);
 	r = read(f1, buffer, 1024);
 	f2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	do {
+	do{
 		if (f1 == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
-
 		w = write(f2, buffer, r);
 		if (f2 == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, 
+					"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
-
 		r = read(f1, buffer, 1024);
 		f2 = open(argv[2], O_WRONLY | O_APPEND);
-	} while (r > 0);
-
+	}while (r > 0);
 	free(buffer);
-
 	o1 = close(f1);
 	if (o1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o1);
 		exit(100);
 	}
-
 	o2 = close(f2);
-	if (o2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o2);
-		exit(100);
-	}
-
+        if (o2 == -1)
+        {
+                dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o2);
+                exit(100);
+        }
 	return (0);
 }
-
