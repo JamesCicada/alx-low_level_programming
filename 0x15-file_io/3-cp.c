34932ad0c72c4584e7de54ed7644dd9705838ded
JamesCicada <mabdoox@gmail.com>
2023-07-13 05:30:45 -0700

    Mandaory Tasks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,72 +1,100 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include "main.h"
 
-#define BUFFER_SIZE 1024
-
 /**
- * error_exit - Prints an error message and exits the program.
- * @msg: The error message to print.
+ * read_from_file - Read from file and store in buffer
+ * @filename: Name of the file to read
+ * @buffer: Buffer to store the read content
+ *
+ * Return: Number of bytes read, or -1 on failure
  */
-void error_exit(const char *msg)
+ssize_t read_from_file(const char *filename, char *buffer)
 {
-	dprintf(STDERR_FILENO, "%s\n", msg);
-	exit(EXIT_FAILURE);
-}
+	int fd, r;
 
-/**
- * cp_file - Copies the content of one file to another.
- * @file_from: The name of the source file.
- * @file_to: The name of the destination file.
- */
-void cp_file(const char *file_from, const char *file_to)
-{
-	int fd_from, fd_to, bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (-1);
 
-	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1)
-		error_exit("Error: Can't read from file");
-
-	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
-		error_exit("Error: Can't write to file");
-
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	r = read(fd, buffer, 1024);
+	if (r == -1)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-			error_exit("Error: Can't write to file");
+		close(fd);
+		return (-1);
 	}
 
-	if (bytes_read == -1)
-		error_exit("Error: Can't read from file");
-
-	if (close(fd_from) == -1)
-		error_exit("Error: Can't close fd");
-	if (close(fd_to) == -1)
-		error_exit("Error: Can't close fd");
+	close(fd);
+	return (r);
 }
 
 /**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of command-line argument strings.
+ * write_to_file - Write content from buffer to file
+ * @filename: Name of the file to write to
+ * @buffer: Buffer containing the content to write
+ * @length: Length of the content to write
  *
- * Return: Always 0.
+ * Return: 1 on success, -1 on failure
+ */
+int write_to_file(const char *filename, char *buffer, ssize_t length)
+{
+	int fd, w;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd == -1)
+		return (-1);
+
+	w = write(fd, buffer, length);
+	if (w == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
+
+/**
+ * main - Copies the contents of a file to another file
+ * @argc: Number of arguments
+ * @argv: Argument vector
+ *
+ * Return: 0 on success
  */
 int main(int argc, char **argv)
 {
+	char *buffer;
+	ssize_t r, w;
+
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	cp_file(argv[1], argv[2]);
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't allocate memory\n");
+		exit(99);
+	}
 
+	r = read_from_file(argv[1], buffer);
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buffer);
+		exit(98);
+	}
+
+	w = write_to_file(argv[2], buffer, r);
+	if (w == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buffer);
+		exit(99);
+	}
+
+	free(buffer);
 	return (0);
 }
 
