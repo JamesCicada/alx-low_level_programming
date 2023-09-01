6572196f1be6a24e2d0cfec4bc1d41cb86850a86
JamesCicada <mabdoox@gmail.com>
2023-09-01 13:29:08 -0700

    Mandatory Tasks

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * set_bit - Sets the value of a bit to 1 at a given index.
+ * clear_bit - Clears (sets to 0) the value of a bit at a given index.
  * @n: A pointer to the number to modify.
- * @index: The index of the bit to set (0-based).
+ * @index: The index of the bit to clear (0-based).
  *
  * Return: 1 if it worked, or -1 if an error occurred.
  */
