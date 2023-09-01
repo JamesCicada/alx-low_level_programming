678ae24cd3eb1ffa44eccd7833991eae7e739025
JamesCicada <mabdoox@gmail.com>
2023-09-01 13:42:21 -0700

    Mandatory Tasks

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness of the system.
+ *
+ * Return: 0 if big endian, 1 if little endian.
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+	return (*((char *)&n) == 1);
+}
