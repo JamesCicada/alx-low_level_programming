709ed34cb237ead14895c93f2277748a9d645bf2
JamesCicada <mabdoox@gmail.com>
2023-09-01 06:35:29 -0700

    fixing problems

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,24 +11,5 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int m = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	int starts = 0;
-
-	while (m > 0)
-	{
-		if (n & m)
-		{
-			putchar('1');
-		}
-		else if (starts)
-		{
-			putchar('0');
-		}
-		m >>= 1;
-	}
-
-	if (!starts)
-	{
-		putchar('0');
-	}
+	n > 1 ? print_binary(b >> 1) : _putchar((b & 1) + '0');
 }
